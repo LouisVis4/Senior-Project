4734867bbb80c654a247af3fd813734d316d45e7
Louis Visotcky <louisvisotcky@gmail.com>
2023-12-09 16:49:54 -0500

    scraper .5

--- python/scrape.py
+++ python/scrape.py
@@ -1,5 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
+import lxml
 
 html_text = requests.get('https://tikisgeckos.com/collections/crested-geckos-1').text
-soup = BeautifulSoup(html_text, 'lmxl')
+soup = BeautifulSoup(html_text, 'html.parser')
+geckos = soup.find_all('div', class_ = 'grid__item small--one-half medium-up--one-fifth')
+for gecko in geckos:
+    gecko_name = gecko.find('div', class_ = 'product-card__name').text.replace(' ','').replace('\n','')
+    gecko_price = gecko.find('div', class_ = 'product-card__price').text.replace(' ','').replace('\n','')
+    print(gecko_name)
+    print(gecko_price)
