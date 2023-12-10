e30d299dbe59477926aa5ad78aad7fc3f490900e
Louis Visotcky <louisvisotcky@gmail.com>
2023-12-10 14:03:17 -0500

    scrape works 1

--- python/scrape.py
+++ python/scrape.py
@@ -5,8 +5,11 @@ import lxml
 html_text = requests.get('https://tikisgeckos.com/collections/crested-geckos-1').text
 soup = BeautifulSoup(html_text, 'html.parser')
 geckos = soup.find_all('div', class_ = 'grid__item small--one-half medium-up--one-fifth')
-for gecko in geckos:
+for index, gecko in enumerate(geckos):
     gecko_name = gecko.find('div', class_ = 'product-card__name').text.replace(' ','').replace('\n','')
     gecko_price = gecko.find('div', class_ = 'product-card__price').text.replace(' ','').replace('\n','')
-    print(gecko_name)
-    print(gecko_price)
+    with open(f'posts/{index}.txt', 'w') as f:
+        f.write(f"Gecko Name: {gecko_name.strip()}\n")
+        f.write(f"Gecko Price: {gecko_price.strip()}")
+    print(f'File saved: {index}')
+
